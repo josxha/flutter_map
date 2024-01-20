16d623fbe91d564d8c80ebc7f75f013f0651d0ff
Joscha <34318751+josxha@users.noreply.github.com>
2024-01-20 21:34:49 +0100

    trackpad zoom center on cursor

--- lib/src/map/gestures/services/trackpad_zoom.dart
+++ lib/src/map/gestures/services/trackpad_zoom.dart
@@ -8,33 +8,62 @@ part of 'base_services.dart';
 /// See https://docs.flutter.dev/release/breaking-changes/trackpad-gestures
 class TrackpadZoomGestureService extends _BaseGestureService {
   double _lastScale = 1;
+  Offset? _startLocalFocal;
+  LatLng? _startFocalLatLng;
+  Offset? _lastLocalFocal;
 
   TrackpadZoomGestureService({required super.controller});
 
   double get _velocity => _options.interactionOptions.trackpadZoomVelocity;
 
+  bool get _moveEnabled => _options.interactionOptions.enabledGestures.drag;
+
   void start(PointerPanZoomStartEvent details) {
     _lastScale = 1;
+    _startLocalFocal = details.localPosition;
+    _startFocalLatLng = _camera.offsetToCrs(_startLocalFocal!);
+    _lastLocalFocal = _startLocalFocal;
   }
 
   void update(PointerPanZoomUpdateEvent details) {
+    if (_startFocalLatLng == null ||
+        _startLocalFocal == null ||
+        _lastLocalFocal == null) return;
     if (details.scale == _lastScale) return;
     final scaleFactor = (details.scale - _lastScale) * _velocity + 1;
-    print(scaleFactor);
 
     final tmpZoom = _camera.zoom * scaleFactor;
     final newZoom = _camera.clampZoom(tmpZoom);
 
-    // TODO: calculate new center
+    LatLng newCenter = _camera.center;
+    if (_moveEnabled) {
+      math.Point<double> newCenterPt;
+
+      final oldCenterPt = _camera.project(_camera.center, newZoom);
+      final newFocalLatLong = _camera.offsetToCrs(_startLocalFocal!, newZoom);
+      final newFocalPt = _camera.project(newFocalLatLong, newZoom);
+      final oldFocalPt = _camera.project(_startFocalLatLng!, newZoom);
+      final zoomDifference = oldFocalPt - newFocalPt;
+      final moveDifference =
+          _rotateOffset(_camera, _startLocalFocal! - _lastLocalFocal!);
+
+      newCenterPt = oldCenterPt + zoomDifference + moveDifference.toPoint();
+      newCenter = _camera.unproject(newCenterPt, newZoom);
+    }
 
     _lastScale = details.scale;
+    _lastLocalFocal = details.localPosition;
     controller.moveRaw(
-      _camera.center,
+      newCenter,
       newZoom,
       hasGesture: true,
       source: MapEventSource.trackpad,
     );
   }
 
-  void end(PointerPanZoomEndEvent details) {}
+  void end(PointerPanZoomEndEvent details) {
+    _startLocalFocal = null;
+    _startFocalLatLng = null;
+    _lastLocalFocal = null;
+  }
 }
